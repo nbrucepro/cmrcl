df772d3bdc2d46e2c8bccbc408114faa8c5f17f9
nbrucepro <nezabruce@gmail.com>
2025-10-08 00:31:37 +0800

    gongzi

--- src/app/(dashboard)/inv/inventory/page.tsx
+++ src/app/(dashboard)/inv/inventory/page.tsx
@@ -4,8 +4,8 @@ import { useGetProductsQuery } from "@/state/api";
 import Header from "@/app/(components)/Header";
 import Loader from "@/app/(components)/common/Loader";
 import { useState } from "react";
-import TransactionModal from "../(components)/inventory/TransactionModal";
-import ProductTable from "../(components)/inventory/ProductTable";
+import TransactionModal from "../../../(components)/inventory/TransactionModal";
+import ProductTable from "../../../(components)/inventory/ProductTable";
 
 export default function InventoryPage() {
   const { data: products, isError, isLoading, refetch } = useGetProductsQuery();
