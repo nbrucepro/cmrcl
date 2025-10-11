d2b788637ebc759d78e329eb4871265f6082f834
nbrucepro <nezabruce@gmail.com>
2025-10-11 16:59:18 +0800

    compact2

--- src/app/(dashboard)/dashboardWrapper.tsx
+++ src/app/(dashboard)/dashboardWrapper.tsx
@@ -4,12 +4,24 @@ import React, { useEffect } from "react";
 import Navbar from "@/app/(components)/Navbar";
 import Sidebar from "@/app/(components)/Sidebar";
 import StoreProvider, { useAppSelector } from "./redux";
+import { useRouter } from "next/router";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const isSidebarCollapsed = useAppSelector(
     (state) => state.global.isSidebarCollapsed
   );
   const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/adminlogin");
+    } else {
+      router.push("/inv/dashboard");
+    }
+  }, [router]);
 
   useEffect(() => {
     if (isDarkMode) {
